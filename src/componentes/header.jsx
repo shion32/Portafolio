b0bc96f9a909ee2d290b222ec47febd21154a0fa
shion32 <annysofiamontano@gmail.com>
2024-10-24 17:00:44 -0500

    arregle la flor

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import florGrande from '../img/florGrande.png';
-import florpequeña from '../img/florpequeña.png';
+import florPequeña from '../img/florpequeña.png';
 
 export const Header = () => {
   return (
     <div id='home' className='header'>
       <img className="florG" src={florGrande} alt="grande" />
-      <img className="florP" src={florpequeña} alt="pequeña" />
+      <img className="florP" src={florPequeña} alt="pequeña" />
       <div className='letra2'>
         <h2>holaa, soy Anny Montaño software developer</h2>
         <div className='letra'>
