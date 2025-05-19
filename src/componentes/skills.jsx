d3fb06391acf43907333d7bfe602a74e10fea869
shion32 <annysofiamontano@gmail.com>
2025-05-19 18:37:05 -0500

    responsive

--- src/componentes/skills.jsx
+++ src/componentes/skills.jsx
@@ -5,31 +5,31 @@ export const Skills = () => {
     <section id="skills" className="section">
     <h3 className="tit  text-center ">Skills</h3>
       <div className="card flex flex-row  justify-center">
-        <div class="targetas mb-4 dark:bg-gray-800 dark:border-gray-700">
+        <div class="targetas mb-4 ">
           <div class="flex justify-end px-4 pt-2"></div>
           <div class="flex flex-col items-center pb-2">
             <div class="flex items-center justify-center w-32 h-32 mb-3 bg-white border border-gray-200 rounded-full shadow-lg hover:bg-[#a1e2d4]">
               <i class="fa-solid fa-database text-gray-900 text-4xl"></i>
             </div>
-            <h5 class="textC   mb-3 text-xl font-['Cinzel_Decorative'] font-bold text-gray-900 dark:text-white  ">
+            <h5 class="textC   mb-3 text-xl font-['Cinzel_Decorative'] font-bold text-gray-900   ">
               base de datos 
             </h5>
-            <span class=" text-center  dark:text-gray-400">
+            <span class=" text-center  ">
             Gestión y optimización de bases de datos, garantizando la calidad y eficiencia en cada etapa del proceso. 
             </span>
           </div>
         </div>
 
-        <div class="targetas mb-4 dark:bg-gray-800 dark:border-gray-700">
+        <div class="targetas mb-4 ">
           <div class="flex justify-end px-4 pt-2"></div>
           <div class="flex flex-col items-center pb-2">
             <div class="flex items-center justify-center w-32 h-32 mb-3 bg-white border border-gray-200 rounded-full shadow-lg hover:bg-[#a1e2d4]">
               <i class="fa-solid fa-code text-gray-900 text-4xl"></i>
             </div>
-            <h5 class=" textC mb-3 text-xl font-medium text-gray-900 dark:text-white">
+            <h5 class=" textC mb-3 text-xl font-medium text-gray-900 ">
               backend
             </h5>
-            <span class=" text-center  dark:text-gray-400 ">
+            <span class=" text-center   ">
             limplementacion de la logica en el analisis y ejecucion del software, aplicando buenas practicas de desarrollo
             </span>
           </div>
