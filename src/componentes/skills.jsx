18924c6d45d22379323b8cf9f5b89c84e4772429
shion32 <annysofiamontano@gmail.com>
2025-05-19 19:09:35 -0500

    responsivev1

--- src/componentes/skills.jsx
+++ src/componentes/skills.jsx
@@ -36,30 +36,30 @@ export const Skills = () => {
         </div>
 
 
-        <div class="targetas mb-4 dark:bg-gray-800 dark:border-gray-700">
+        <div class="targetas mb-4 ">
           <div class="flex justify-end px-4 pt-2"></div>
           <div class="flex flex-col items-center pb-2">
             <div class="flex items-center justify-center w-32 h-32 mb-3 bg-white border border-gray-200 rounded-full shadow-lg hover:bg-[#a1e2d4]">
               <i class="fa-solid fa-brush text-gray-900 text-4xl"></i>
             </div>
-            <h5 class=" textC  mb-3 text-xl font-medium text-gray-900 dark:text-white">
+            <h5 class=" textC  mb-3 text-xl font-medium text-gray-900 ">
             front-end
             </h5>
-            <span class=" text-center  dark:text-gray-400">
+            <span class=" text-center  ">
             diseño con enfoque responsivo, orientado a la máxima calidad y optimización del aplicativo
             </span>
           </div>
         </div>
-        <div class="targetas mb-4 dark:bg-gray-800 dark:border-gray-700">
+        <div class="targetas mb-4 ">
           <div class="flex justify-end px-4 pt-2"></div>
           <div class="flex flex-col items-center pb-2">
             <div class="flex items-center justify-center w-32 h-32 mb-3 bg-white border border-gray-200 rounded-full shadow-lg hover:bg-[#a1e2d4]">
               <i class="fa-solid fa-users-line text-gray-900 text-4xl"></i>
             </div>
-            <h5 class=" textC  mb-3 text-xl font-medium text-gray-900 dark:text-white">
+            <h5 class=" textC  mb-3 text-xl font-medium text-gray-900 ">
               metodologias agiles
             </h5>
-            <span class=" text-center   dark:text-gray-400 ">
+            <span class=" text-center    ">
             uso de metodologías ágiles y trabajo en equipo, asegurando entregas continuas de resultados de alta calidad.
             </span>
           </div>
